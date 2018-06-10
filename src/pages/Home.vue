85c9b88c04304f81eac072525d8d0bc9b974cb3c
AnnaFlourou <flourou.anna@gmail.com>
2018-06-10 21:53:24 +0200

    vue discovery, manip css + cmp

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,15 +1,26 @@
 <template>
   <main-layout>
-    <p>Welcome home</p>
+    <div class="deco">
+      <p>Welcome home</p>
+      <p>this is a brand new day</p>
+    </div>
   </main-layout>
 </template>
 
 <script>
-  import MainLayout from '../layouts/Main.vue'
+import MainLayout from "../layouts/Main.vue";
 
-  export default {
-    components: {
-      MainLayout
-    }
+export default {
+  components: {
+    MainLayout
   }
+};
 </script>
+<style scoped>
+.deco {
+  font-family: 'Courier New', Courier, monospace;
+  font-size: 4em;
+  color: blue;
+  text-align: center;
+}
+</style>
